21ec8da71da3749b916de71578313ce466943dc4
Marcosware <marcosware.dev@gmail.com>
2025-01-30 19:26:50 -0300

    Last math exercise list

---
+++ src/main/java/Revisao_Math_ExpressoesAritmeticas/Exercicios_ExpressoesAritmeticas.java
@@ -0,0 +1,50 @@
+package Revisao_Math_ExpressoesAritmeticas;
+
+import java.util.Locale;
+
+public class Exercicios_ExpressoesAritmeticas {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        System.out.println("EXERCÍCIOS COM EXPRESSÕES ARITMÉTICAS\n");
+        System.out.println("1. Calcule (10 + 5) * 2.");
+        System.out.printf("Resultado: %d\n\n", (10 + 5) * 2);
+        System.out.println("2. Determine o valor de (20 - 4) / 2.");
+        System.out.printf("Resultado: %d\n\n", (20 - 4) / 2);
+        System.out.println("3. Resolva 15 + 3 * 2.");
+        System.out.printf("Resultado: %d\n\n", 15 + 3 * 2);
+        System.out.println("4. Calcule 100 / (2 + 3).");
+        System.out.printf("Resultado: %d\n\n", 100 / (2 + 3));
+        System.out.println("5. Determine o valor de 25 % 4.");
+        System.out.printf("Resultado: %d\n\n", 25 % 4);
+        System.out.println("6. Resolva (30 - 5) * (2 + 3).");
+        System.out.printf("Resultado: %d\n\n", (30 - 5) * (2 + 3));
+        System.out.println("7. Calcule (50 / 2) + (8 * 3).");
+        System.out.printf("Resultado: %d\n\n", (50 / 2) + (8 * 3));
+        System.out.println("8. Resolva 2 * (3 + 4) - 5.");
+        System.out.printf("Resultado: %d\n\n", 2 * (3 + 4) - 5);
+        System.out.println("9. Determine o valor de 100 - (25 / 5).");
+        System.out.printf("Resultado: %d\n\n", 100 - (25 / 5));
+        System.out.println("10. Calcule (8 + 2) * (6 - 4).");
+        System.out.printf("Resultado: %d\n\n", (8 + 2) * (6 - 4));
+        System.out.println("11. Resolva 45 % (7 + 3).");
+        System.out.printf("Resultado: %d\n\n", 45 % (7 + 3));
+        System.out.println("12. Calcule (12 / 3) + (18 / 6).");
+        System.out.printf("Resultado: %d\n\n", (12 / 3) + (18 / 6));
+        System.out.println("13. Determine o valor de 2^3 + 5 * 2.");
+        System.out.printf("Resultado: %.0f\n\n", Math.pow(2, 3) + (5 * 2));
+        System.out.println("14. Resolva (15 - 5) * (3^2).");
+        System.out.printf("Resultado: %.0f\n\n", (15 - 5) * Math.pow(3, 2));
+        System.out.println("15. Calcule (8 % 3) + (6 / 2).");
+        System.out.printf("Resultado: %d\n\n", (8 % 3) + (6 / 2));
+        System.out.println("16. Resolva (7 + 5) - (3 * 2).");
+        System.out.printf("Resultado: %d\n\n", (7 + 5) - (3 * 2));
+        System.out.println("17. Determine o valor de (9 * 9) - (81 / 9).");
+        System.out.printf("Resultado: %d\n\n", (9 * 9) - (81 / 9));
+        System.out.println("18. Calcule (50 / (5 + 5)) +  10.");
+        System.out.printf("Resultado: %d\n\n", (50 / (5 + 5)) +  10);
+        System.out.println("19. Resolva (16 % 5) * (7 - 3).");
+        System.out.printf("Resultado: %d\n\n", (16 % 5) * (7 - 3));
+        System.out.println("20. Determine o valor de (10 + 5) / (2^2).");
+        System.out.printf("Resultado: %.2f\n\n", (10 + 5) / Math.pow(2, 2));
+    }
+}
