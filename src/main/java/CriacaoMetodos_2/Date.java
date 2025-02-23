e2330112a3dafa59e173d5173569fedab0bd21db
Marcosware <marcosware.dev@gmail.com>
2025-02-23 14:23:42 -0300

    Date empty constructor added

--- src/main/java/CriacaoMetodos_2/Date.java
+++ src/main/java/CriacaoMetodos_2/Date.java
@@ -8,6 +8,12 @@ public class Date {
     private int month;
     private int year;
 
+    public Date() {
+        this.day = 0;
+        this.month = 0;
+        this.year = 0;
+    }
+
     public Date(int day, int month, int year) {
         this.day = day;
         this.month = month;
