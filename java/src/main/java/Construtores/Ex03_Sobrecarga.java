cc9c56c563398d016337d0e83f14f826bf61b5c7
Marcosware <marcosware.dev@gmail.com>
2025-03-13 20:55:13 -0300

    Construtores Ex03 update

--- java/src/main/java/Construtores/Ex03_Sobrecarga.java
+++ java/src/main/java/Construtores/Ex03_Sobrecarga.java
@@ -5,15 +5,26 @@ import java.util.Scanner;
 public class Ex03_Sobrecarga {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Pessoa pessoa;
-        System.out.println("ABERTURA DE CONTA BANCÁRIA");
-        System.out.print("Nome: ");
-        String nome = sc.nextLine();
-        System.out.print("Idade: ");
-        String idade = sc.nextLine();
-        pessoa = Pessoa.criacaoPessoa(nome, idade);
-        System.out.println("\nCadastro realizado com sucesso!\n");
-        System.out.println("Nome: " + pessoa.getNome());
-        System.out.print("Idade: " + pessoa.getIdade());
+        ContaBancaria contaA, contaB;
+        System.out.println("ABERTURA DE CONTA BANCÁRIA\n(Construtor de três atributos)\n");
+        System.out.print("Nome do Titular: ");
+        String titular = sc.nextLine();
+        System.out.print("Saldo: ");
+        String saldo = sc.nextLine();
+        System.out.print("Tipo de Conta: ");
+        String tipoConta = sc.nextLine();
+        contaA = new ContaBancaria(titular, Double.parseDouble(saldo), tipoConta);
+        System.out.println("\nConta bancária criada com sucesso!\n");
+        System.out.println("Titular da conta: " + contaA.getTitular());
+        System.out.println("Saldo: " + contaA.getSaldo());
+        System.out.print("Tipo de Conta: " + contaA.getTipoConta());
+        System.out.println("\n\nABERTURA DE CONTA BANCÁRIA\n(Construtor de um atributo)");
+        System.out.print("Nome do Titular: ");
+        titular = sc.nextLine();
+        contaB = new ContaBancaria(titular);
+        System.out.println("\nConta bancária criada com sucesso!\n");
+        System.out.println("Titular da conta: " + contaB.getTitular());
+        System.out.println("Saldo: " + contaB.getSaldo());
+        System.out.println("Tipo de conta: " + contaB.getTipoConta());
     }
 }
